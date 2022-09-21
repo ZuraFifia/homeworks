f518f72854f6bc92e776eaa9487db606f8d2ea40
ZuraFifia <fifiazura65@gmail.com>
2022-09-21 16:46:07 +0400

    first commit

---
+++ day 2/group 2/lesson2.py
@@ -0,0 +1,13 @@
+me = "me"
+mqvia = "mqvia"
+zura = "zura"
+gvarad = "gvarad fifia gaxlavart"
+var = "var"
+welp = "15"
+wlis = "wlis"
+chemi = "chemi"
+simagle = "simagle 167.8"
+vswavlob = "vswavlob Goal-Oriented academiashi"
+
+
+print(me + " " + mqvia + " " + zura + " " + gvarad + " " + var + " " + welp + " " + wlis + " " + chemi + " " + simagle + " " + vswavlob)
