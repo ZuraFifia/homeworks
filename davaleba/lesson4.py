c46b021383f452d7f3b39b4a7b6df166eb700aa3
ZuraFifia <fifiazura65@gmail.com>
2022-10-01 20:40:09 +0400

    davaleba

---
+++ davaleba/lesson4.py
@@ -0,0 +1,10 @@
+my_name = "zura"
+my_surname = "fifia"
+my_pet_name = "gela"
+my_age = "16"
+my_height = "170"
+my_weight = "83"
+my_living_place = "sagrejo"
+my_sentence = "my name is {0} {1} im {3} my pet name is {2} my height is {4} weight {5} i live in {6}".format(my_name,my_surname,my_pet_name,my_age,my_height,my_weight,my_living_place)
+
+print(my_sentence)
